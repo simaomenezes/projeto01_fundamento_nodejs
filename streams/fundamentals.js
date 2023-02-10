bc3a553ee26ebde57f16d43e8236a046a2bbc229
Simão Neto <simaomenezesneto@gmail.com>
2023-02-10 22:19:32 +0000

    feat: First Streams

---
+++ streams/fundamentals.js
@@ -0,0 +1,26 @@
+// Streams ->
+
+// process.stdin.pipe(process.stdout)
+
+import { Readable } from 'node:stream'
+
+class OnTohundredStream extends Readable {
+    index = 1
+
+    _read() {
+        const i = this.index++
+
+        setTimeout(() => {
+            if(i > 100) {
+                this.push(null)
+            } else {
+                const buf = Buffer.from(String(i))
+    
+                this.push(buf)
+            }
+        }, 1000)
+    }
+}
+
+new OnTohundredStream()
+    .pipe(process.stdout)
