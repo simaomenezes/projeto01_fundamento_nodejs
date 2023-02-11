346b678f06ac984cabaa72dcce6061c85c08dff8
Simão Neto <simaomenezesneto@gmail.com>
2023-02-11 12:29:52 +0000

    feat: Create database json

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import http from 'http'
+import { Database } from './database.js';
 import { json } from './middlewares/json.js';
 
 /*
@@ -17,7 +18,7 @@ Cabeçalhos (Requisição/resposta) => Metadados
 HTTP Status code
 */
 
-const users = [];
+const database = new Database()
 
 const server = http.createServer(async (req, res) => {
 
@@ -26,17 +27,20 @@ const server = http.createServer(async (req, res) => {
     await json(req, res)
 
     if(method === 'GET' && url === '/users') {
+        const users = database.select('users')
 
         return res.end(JSON.stringify(users));
     }
 
     if(method === 'POST' && url === '/users') {
         const {name, email } = req.body
-        users.push({
+        const user = {
             id: 1,
             name,
             email,
-        })
+        }
+
+        database.insert("users", user)
         return res.writeHead(201).end();
     }
 
