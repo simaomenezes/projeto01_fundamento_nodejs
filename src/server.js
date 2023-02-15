7076ed139e13c1d925926a3862ff21d7c158d5a2
Simão Neto <simaomenezesneto@gmail.com>
2023-02-15 21:45:16 +0000

    feat: Route && QueryParameters, Work with Regex, regex of parameters, delete record

--- src/server.js
+++ src/server.js
@@ -16,16 +16,26 @@ Json - JavaScript Object Notation
 Cabeçalhos (Requisição/resposta) => Metadados
 
 HTTP Status code
+
+
+Query Parameters
+Route Parameters
+Request Body
+
 */
 
 const server = http.createServer(async (req, res) => {
     const { method, url } = req;
     await json(req, res)
     const route = routes.find(route => {
-        return route.method === method && route.path === url
+        return route.method === method && route.path.test(url)
     }) 
 
     if(route) {
+        const routeParams = req.url.match(route.path)
+
+        req.params = { ...routeParams.groups }
+        console.log(req.params)
        return route.handler(req, res)
     }
     return res.writeHead(404).end()
