a8fa119fc416470e98e96c260fa07645bc26d718
Simão Neto <simaomenezesneto@gmail.com>
2023-02-16 20:25:33 +0000

    feat: update use PUT

--- src/server.js
+++ src/server.js
@@ -33,9 +33,7 @@ const server = http.createServer(async (req, res) => {
 
     if(route) {
         const routeParams = req.url.match(route.path)
-
         req.params = { ...routeParams.groups }
-        console.log(req.params)
        return route.handler(req, res)
     }
     return res.writeHead(404).end()
