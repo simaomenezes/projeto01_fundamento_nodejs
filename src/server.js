812fa82884b9a1267e96fc73e16122fc25cb946b
Simão Neto <simaomenezesneto@gmail.com>
2023-02-16 20:44:47 +0000

    feat: Query Params capture

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http'
 import { json } from './middlewares/json.js';
 import { routes } from './route.js';
+import { extractQueryParams } from './utils/extract-query-params.js';
 
 /*
 GET     -> BUSCAR UM RECURSO DO BACK-END
@@ -33,7 +34,9 @@ const server = http.createServer(async (req, res) => {
 
     if(route) {
         const routeParams = req.url.match(route.path)
-        req.params = { ...routeParams.groups }
+        const { query, ...params } = routeParams.groups
+        req.params = params
+        req.query = query ? extractQueryParams(query): {}
        return route.handler(req, res)
     }
     return res.writeHead(404).end()
